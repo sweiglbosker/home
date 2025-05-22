949bdb233fd26bfe5ba51368f6172a33a369f714
stefan <stefan@s00.xyz>
2025-05-22 00:05:19 -0400

    repo: move to modular nixos configuration

--- form/configuration.nix
+++ form/configuration.nix
@@ -1,136 +1,31 @@
 { config, lib, pkgs, inputs, ... }:
-
 {
-  imports =
-    [ 
+  imports = 
+    [
+      ../nixos
       ./hardware-configuration.nix
     ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.consoleMode = "max";
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  nixpkgs.config.allowUnfree = true;
-
-  networking.hostName = "form"; 
-
-  networking.networkmanager.enable = true;  
-
-  time.timeZone = "America/New_York";
-
-  console = {
-    useXkbConfig = true; # use xkb.options in tty.
+  config.nixos = {
+    username = "stefan";
+    hostname = "form";
+    wifi = false;
   };
 
-  services.xserver.xkb.layout = "us";
-  services.xserver.xkb.variant = "colemak_dh,";
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    wireplumber.enable = true;
-    pulse.enable = true;
-  };
-
-  services.libinput.enable = true;
-
-  programs.zsh = {
-    enable = true;
-    ohMyZsh.enable = true;
-  };
-
-  documentation.dev.enable = true;
-
-  users.users.stefan = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "gamemode" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      unzip
-      tree
-      discord-ptb
-      ungoogled-chromium
-      osu-lazer-bin
-      xfce.thunar
+  config = {
+    hardware.graphics.enable = true;
+    boot.initrd.kernelModules = [ "nvidia" "nvidia_uvm" "nvidia_drm" ];
+    boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+    # boot.kernelParams = [];
+    services.xserver.videoDrivers = ["nvidia"];
+    hardware.nvidia = {
+      modesetting.enable = true;
+      open = true;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
+    };
+    hardware.opengl.extraPackages = with pkgs; [
+      vaapiVdpau
     ];
-    shell = pkgs.zsh;
   };
-
-  #programs.firefox.enable = true;
-  programs.foot.enable = true;
-  programs.sway.enable = true;
-  programs.steam.enable = true;
-
-  security = {
-    polkit.enable = true;
-    rtkit.enable = true;
-    doas = {
-      enable = true;
-      extraRules = [{
-        users = ["stefan"];
-  #      keepEnv = true;
-        persist = true;
-      }];
-    };
-  };
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd 'sway --unsupported-gpu'";
-        user = "greeter";
-      };
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    zsh
-    neovim
-    wl-clipboard
-    wmenu
-    gnupg
-    pinentry-qt
-    btop
-    man-pages
-    man-pages-posix
-    wineWowPackages.staging
-    winetricks
-    wineWowPackages.waylandFull
-  ];
-
-  environment.pathsToLink = [ "/share/zsh" ];
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-
-  hardware.graphics.enable = true;
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
-  };
-
-  nix = {
-    settings.experimental-features = [ "nix-command" "flakes" ];	
-  };
-
-  system.stateVersion = "24.11";
 }
-
---
+++ nixos/default.nix
@@ -0,0 +1,179 @@
+{ config, lib, pkgs, inputs, ... }:
+let 
+  cfg = config.nixos; 
+in
+{
+  imports = [
+    # ./kmonad.nix
+  ];
+
+  options.nixos = with lib.options; {
+    hostname = mkOption { 
+      type = lib.types.str;
+      description = "Hostname for the system";
+      default = "nixos";
+    };
+
+    timezone = mkOption {
+      type = lib.types.str;
+      description = "Local timezone";
+      default = "America/New_York";
+    };
+
+    username = mkOption {
+      type = lib.types.str;
+      description = "Username for the main user";
+      default = "stefan";
+    };
+
+    shell = mkOption {
+      type = lib.types.package;
+      default = pkgs.zsh;
+      description = "Interactive shell for main user";
+    };
+
+    wifi = mkOption {
+      type = lib.types.bool;
+      description = "Whether to enable wireless networking via NetworkManager";
+      default = true;
+    };
+  };
+
+  config = {
+    # nixos = {
+    # };
+    nix = {
+      settings.experimental-features = [ "nix-command" "flakes" ];
+    };
+
+    time.timeZone = cfg.timezone;
+    nixpkgs.config.allowUnfree = true;
+
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.systemd-boot.consoleMode = "max";
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking = {
+      hostName = cfg.hostname;
+      networkmanager.enable = cfg.wifi;
+    };
+
+    documentation.dev.enable = true;
+
+    users.users."${cfg.username}" = {
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+        "input" # TODO: remove;
+        "uinput"
+        "networkmanager"
+        "gamemode"
+      ];
+        # ++ (lib.optional cfg.wifi "networkmanager")
+        # ++ (lib.optional cfg.gaming "gamemode");
+      packages = with pkgs; [
+        unzip
+        tree
+        ungoogled-chromium
+      ];
+      shell = cfg.shell;
+    };
+
+    environment.systemPackages = with pkgs; [
+      neovim
+      wl-clipboard
+      wmenu
+      gnupg
+      pinentry-qt
+      btop
+      man-pages
+      man-pages-posix
+      cage
+      zsh
+    ]; # TODO
+
+
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.greetd}/bin/agreety --cmd 'sway --unsupported-gpu'"; 
+        };
+      };
+    };
+
+    programs.zsh = {
+      enable = true;
+      # TODO
+      shellInit = ''
+        # autoload -Uz add-zsh-hook # avoids an error with system prompt
+      '';
+      # promptInit = "";
+    };
+    # TODO
+    programs.foot.enable = true;
+    programs.foot.enableZshIntegration = false; # TODO: https://github.com/NixOS/nixpkgs/pull/409627
+    programs.sway.enable = true;
+    programs.steam = {
+      enable = true;
+    };
+    programs.gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+
+    security = {
+      polkit.enable = true;
+      rtkit.enable = true;
+      doas = {
+        enable = true;
+        extraRules = [{
+          users = ["${cfg.username}"];
+          keepEnv = true;
+          persist = true;
+        }];
+      };
+    };
+    
+    services.xserver.xkb.layout = "us";
+    services.xserver.xkb.variant = "colemak_dh,";
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      wireplumber.enable = true;
+      pulse.enable = true;
+    };
+
+    services.libinput.enable = true;
+
+    # environment.pathsToLink = [ "/share/zsh" ];
+    console = {
+      earlySetup = true;
+      useXkbConfig = true;
+      colors = [
+        "0f0f0f"
+        "ac8a8c"
+        "8aac8b"
+        "aca98a"
+        "8f8aac"
+        "ac8aac"
+        "8aabac"
+        "cacaca"
+        "4c4c4c"
+        "ac8a8c"
+        "8aac8b"
+        "aca98a"
+        "8f8aac"
+        "ac8aac"
+        "8aabac"
+        "f0f0f0"
+      ];
+    };
+
+    system.stateVersion = "24.11";
+  };
+
+
+}
