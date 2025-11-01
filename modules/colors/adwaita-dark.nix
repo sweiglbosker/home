a0ab706a3e885cb7ea7166189d99b0319498110c
Stefan Weigl-Bosker <stefan@s00.xyz>
2025-11-01 16:16:04 -0400

    colors/adwaita-dark: refactor

--- modules/colors/adwaita-dark.nix
+++ modules/colors/adwaita-dark.nix
@@ -20,12 +20,11 @@ in
     modules.scheme = {
       name = "base16-adwaita-dark";
       base00 = "#1d1d20";
-      base01 = "#2a2a2a";
-      base03 = "#303030";
-      base02 = "#383838";
+      base01 = "#242428";
+      base02 = "#303030";
+      base03 = "#383838";
       base04 = "#5e5c64";
-      # base04 = "#383838";
-      base05 = "#ffffff";
+      base05 = "#DEDDDA";
       base06 = "#c0bfbc";
       base07 = "#f6f5f4";
       base08 = "#c01c28";
@@ -36,7 +35,7 @@ in
       base0D = "#1c71d8";
       base0E = "#813d9c";
       base0F = "#a51d2d";
-      base10 = "#000000";
+      base10 = "#121212";
       base11 = "#000000";
       base12 = "#ed333b";
       base13 = "#f8e45c";
