8a2f89d27e3d96f87185a0805f0c58a6e583c9a8
Stefan Weigl-Bosker <stefan@s00.xyz>
2025-12-24 13:47:09 -0500

    tblgen_lsp_server: fix compile db detection

--- modules/neovim/nvim/lua/config/lsp/tblgen_lsp_server.lua
+++ modules/neovim/nvim/lua/config/lsp/tblgen_lsp_server.lua
@@ -1,5 +1,19 @@
+local function get_command()
+	local cmd = { ((vim.env.LLVM_DIR and (vim.env.LLVM_DIR .. "/bin/")) or "") .. "tblgen-lsp-server" }
+	local files = vim.fs.find(
+		{ "tablegen_compile_commands.yml", "build/tablegen_compile_commands.yml" },
+		{ path = vim.fn.expand("%:p:h"), upward = true, type = "file" }
+	)
+	if #files > 0 then
+		local file = files[1]
+		table.insert(cmd, "--tablegen-compilation-database=" .. file)
+	end
+
+	return cmd
+end
+
 return {
-  cmd = { ((vim.env.LLVM_DIR and (vim.env.LLVM_DIR .. "/bin/")) or "") .. "tblgen-lsp-server" },
+  cmd = get_command(),
   filetypes = { 'tablegen' },
-  root_markers = { '.git' }
+  root_markers = { 'tablegen_compile_commands.yml', '.git' }
 }
